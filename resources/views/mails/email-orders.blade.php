5bccb1e6d7bd3432f0c2b81e93a35e203d169d13
dalin.hoem <dalin.hoem@mango-byte.com>
2021-12-08 12:08:41 +0700

    improve email template

--- resources/views/mails/email-orders.blade.php
+++ resources/views/mails/email-orders.blade.php
@@ -4,13 +4,15 @@
 <title>Your Email</title>
 </head>
 <body>
-<h1>Dear{{$email}},</h1>
+<h1>Dear,</h1> <h3><i>{{$email}}</i></h3>
 <p>
-
     We've received your order and will get started on it right away.
+</p>
+<p>
     You can expect to received a shipping confimation email soon. 
 </p>
 
 <p>Thank you for shppoing with Online Phone Sale!</p>
+{{ url('http://127.0.0.1:8000/home', []) }}
 </body>
 </html>
