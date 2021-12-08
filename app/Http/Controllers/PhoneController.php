1c5552d92b028657e770a15f6ba4930c4b609f2b
sinat nang <sinatnang1998@gmail.com>
2021-12-08 10:29:00 +0700

    update phone for upload image

--- app/Http/Controllers/PhoneController.php
+++ app/Http/Controllers/PhoneController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Phone;
 use Illuminate\Http\Request;
+use File;
+use Validator;
 
 class PhoneController extends Controller
 {
@@ -42,11 +44,15 @@ class PhoneController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required',
-            'description' => 'required',
-            'price' => 'required',
-            'img' => 'required',
+            'name' => 'required|max:20|min:3',
+            'description' => 'required|max:500|min:10',
+            'price' => 'required|integer',
+            'img' => 'required|mimes:jpg,jpeg,png,gif',
         ]);
+        $image = $request->file('img');
+        $upload = $image->getClientOriginalName();
+        $path = 'image/phones/';
+        $path = move_uploaded_file($image->getPathName(), $path. $upload);
 
         $user_id = Auth::id();
         $data = [
@@ -54,9 +60,10 @@ class PhoneController extends Controller
             'name' => $request->name,
             'description' => $request->description,
             'price' => $request->price,
-            'img' => $request->img,
+            'img'=> $upload,
         ];
-        $phone = phone::create($data);
+        
+        $phone = Phone::create($data);
         return response()->json([
 			"success" => true,
 			"message" => "phone created successfully.",
@@ -113,25 +120,45 @@ class PhoneController extends Controller
     {
         $request->validate([
             'name' => 'required',
-            'description' => 'required',
+            'description' => 'required|max:500|min:10',
             'price' => 'required',
-            'img' => 'required',
+            'img' => 'required|mimes:jpg,jpeg,png,gif',
         ]);
-        $phone = Phone::find($id);
+        $image = $request->file('img');
+        $upload = $image->getClientOriginalName();
+        $path = 'image/phones/';
+        $path = move_uploaded_file($image->getPathName(), $path. $upload);
+        $phones = Phone::find($id);
         $user_id = Auth::id();
         $data = [
             'user_id' => $user_id,
             'name' => $request->name,
             'description' => $request->description,
             'price' => $request->price,
-            'img' => $request->img,
+            'img' => $upload,
         ];
-        $phone->update($data);
+        $phones->update($data);
         return response()->json([
 			"success" => true,
-			"message" => "Product updated successfully.",
+			"message" => "Order updated successfully.",
 			"data" => $data
 		]);
+        // $user_id = Auth::id();
+        // $phone = Phone::find($id);
+        // dd($phone);
+        // $data = [
+        //     'user_id' => $user_id,
+        //     'name' => $request->name,
+        //     'description' => $request->description,
+        //     'price' => $request->price,
+        //     'img' => $request->img,
+        // ];
+        // $phone->update($data);
+        // return response()->json([
+		// 	"success" => true,
+		// 	"message" => "Product updated successfully.",
+		// 	"data" => $data
+		// ]);
     }
 
     /**
